5accca7ebffb18516e54922c2bd3ab8caa3b0ea6
Aleks Nazarenko <nazaren.aleks@gmail.com>
2025-09-02 11:31:50 +0300

    add array examples

--- index.php
+++ index.php
@@ -1,29 +1,40 @@
 <?php
 
-echo "Hello World!\n";
-echo ("Hello World!\n");
-
-$var = 1;
-echo $var;
-echo 'Var is $var';
-echo "\nVar is $var\n";
-echo $var . "\n";
-$num = 1;
-$num = $num + 1;
-$num++;
-$num--;
-$num += 1;
-$num -= 1;
-$num *= 2;
-$num /= 3;
-$num = 13;
-$num %= 5;
-$num = 1.3;
-$num = 3;
-$num **= 2;
-$num = true;
-$num = false;
-$bar;
-$bar = null;
-
-var_dump($bar);
+$array = array(1, 2, 3, 4, 5);
+$array = [1, 2, 3, 4, 5];
+$array = [
+    1, 
+    'hello', 
+    true, 
+    null, 
+    [1, 2, 3]
+];
+$array = [
+    'name' => 'John',
+    'age' => 30,
+    'city' => 'New York'
+];
+$array = [
+    1,
+    2,
+    3,
+    'name' => 'John',
+    88,
+    'age' => 30,
+    1000=> 3,
+    'city' => 'New York',
+    4,
+    5,
+];
+var_dump($array['name']);
+$array = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+var_dump($array[1][2]);
+$array = [1, 2, 3];
+array_push($array, 4);
+array_push($array, 5, 6, 7);
+$array[] = 8;
+var_dump($array);
